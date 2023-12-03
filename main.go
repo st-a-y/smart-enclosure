d84bdf1c01d37e91473aea5768b25d351b0da62e
Alexander Stukov <stukov.a.y@gmail.com>
2023-12-03 23:42:42 +0300

    Adjust structure

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/st-a-y/smart-enclosure/handlers"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/temperature", GetTemperature)
+	router.GET("/temperature", handlers.GetTemperature)
 
 	router.Run("localhost:8080")
 }
