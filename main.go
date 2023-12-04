103452c83b75ba5e9cfe90e7c6a4ece3c15dddc4
Alexander Stukov <stukov.a.y@gmail.com>
2023-12-04 22:42:20 +0300

    More dummy routes

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/st-a-y/smart-enclosure/handlers"
 )
 
 func main() {
 	router := gin.Default()
+
 	router.GET("/temperature", handlers.GetTemperature)
+	router.GET("/humidity", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.POST("/create-tag", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.POST("/add-condition", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
 
 	router.Run("localhost:8080")
 }
